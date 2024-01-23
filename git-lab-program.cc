12ea05ced81dcb869c7ffc17076f31930135e54d
Chaas11 <nh462921@ohio.edu>
2024-01-23 17:39:04 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+* File: git-lab-program.cc
+*Author: Conrad Haas
+Date: 1/23/2024
+Description: This code will display a message.
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv []) {
+
+cout << "Hello Git!!" << endl;
+return 0;
+}
